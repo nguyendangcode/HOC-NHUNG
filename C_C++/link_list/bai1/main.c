665515e65a9fe2bb5a9b1fa088d656ae28443113
nguyen123 <dinh2671@gmail.com>
2025-08-26 23:37:16 +0700

    oke

--- C_C++/link_list/bai1/main.c
+++ C_C++/link_list/bai1/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+struct sinh_vien
+{
+    int mssv[20];
+    char Ho_ten[50];
+    float gpa ;
+    struct sinh_vien *next;
+};
+typedef  struct sinh_vien SV;
 int main() {
-    printf("Hello World");
+    SV a,b;
+    a.gpa=7.4;
+    a.next=&b;
+    b.gpa=7.8;
+    printf("%0.1f",a.next->gpa);
     return 0;
 }
---
+++ C_C++/link_list/bai2/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("Hello World");
+    int *a;
+    int b=10;
+    a=&b;
+    return 0;
+}
